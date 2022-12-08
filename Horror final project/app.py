8c58b26eeaa6c27f6c00fa6432fc6481cad28abd
adhuang1106 <69485502+adhuang1106@users.noreply.github.com>
2022-12-08 17:45:00 -0600

    Add files via upload

--- Horror final project/app.py
+++ Horror final project/app.py
@@ -6,6 +6,8 @@ def login():
     error = None
     if request.method == 'POST':
         if request.form['password'] != 'tao666tao':
+            if request.form['password'] == 'grplqhphxvoxflihu':
+                return redirect(url_for('blog') ,code=307)
             error = 'Invalid Credentials. Please try again.'
         elif request.form['password'] == 'tao666tao':
             return render_template('desktop.html')
@@ -15,9 +17,15 @@ def login():
 
 @app.route('/folder', methods=['GET','POST'])
 def folder():
-    if request.method == 'GET':
+    if request.method == 'POST':
         return render_template('folder.html')
-    return render_template('folder.html')
+    return redirect(url_for('login'))
+
+@app.route('/blog', methods=['GET','POST'])
+def blog():
+    if request.method =='POST':
+        return render_template('blog.html')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.debug = True
